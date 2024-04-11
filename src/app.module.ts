76b21954d696b4e72539e1e6700213f8b040da24
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-04-11 15:45:26 -0300

    Remove PrismaService from providers in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from "@nestjs/common";
-import { PrismaService } from "./prisma/prisma.service";
-
 @Module({
   imports: [],
   controllers: [],
-  providers: [
-    PrismaService
-  ],
+  providers: []
 })
 export class AppModule {}
