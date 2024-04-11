858724223993fbc3147e3344766e28dc7ec8e3fc
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-04-11 14:16:42 -0300

    Config validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { ValidationPipe } from "@nestjs/common";
 
 
 (async function() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({ origin: "*" });
+  app.useGlobalPipes(new ValidationPipe());
   
   await app.listen(3000);
 })();
