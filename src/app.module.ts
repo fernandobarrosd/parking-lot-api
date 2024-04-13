b50b57b2ec78ad0e3ab2e7514c857dc343dc399d
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-04-13 17:57:20 -0300

    Config typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from "@nestjs/common";
 import { VehicleController } from "./vehicle/vehicle-rented.controller";
 import { VehicleService } from "./vehicle/vehicle.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "src/database.db",
+      entities: [],
+      migrations: [],
+      synchronize: false,
+    })
+  ],
   controllers: [
     VehicleController
   ],
